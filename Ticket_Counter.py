b87e6be7dd94d5917666869f2d7a3d436912d49d
jlamberson2 <broadblaster@gmail.com>
2024-08-25 14:29:14 -0400

    Added functionality to save to files

--- Ticket_Counter.py
+++ Ticket_Counter.py
@@ -7,12 +7,13 @@ from tkinter import *
 import tkinter.messagebox
 from decimal import Decimal
 
+#import for checking if a file exists in current directory
+import os
+
 
 #main execution of the program
 def main():
     print("In progess...")
-
-#def make_change(total, paid):
 
 
 #class for making/executing the gui
@@ -85,6 +86,7 @@ class gui:
         #calling main loop
         self.main_window.mainloop()
 
+    #Function definition for the change button
     def calculate_change(self):
         #Gets the inputs from the user
         try:
@@ -107,7 +109,91 @@ class gui:
         
 
     def save_transaction(self):
+        file_name = "tickets sold.txt" #current working filename for saved transactions
         print("In progress")
+        #get current directory
+        current_directory = os.getcwd()
+
+        file_location = current_directory + '\\' + file_name
+
+        #used to check if the file exists, if it doesnt, create it
+        #if it does, append it
+        if os.path.exists(file_location):
+            previous_file = open(file_name, 'r')
+
+            #Get the previous tickets from the file
+            current_line = previous_file.readline()
+            student_tickets = gui.get_tickets(current_line)
+
+            current_line = previous_file.readline()
+            alumni_tickets = gui.get_tickets(current_line)
+
+            current_line = previous_file.readline()
+            general_tickets = gui.get_tickets(current_line)
+
+            previous_file.close()
+
+
+            #Saves the data to the file
+            current_file = open(file_name, 'w')
+            #Gets the inputs from the user
+            try:
+                student_tickets += int(self.student_ticket_input.get(1.0, 'end-1c'))
+                alumni_tickets += int(self.alumni_ticket_input.get(1.0, 'end-1c'))
+                general_tickets += int(self.general_ticket_input.get(1.0, 'end-1c'))
+            except ValueError:
+                tkinter.messagebox.showerror("Error", "There must be an input for all ticket fields")
+
+            #writing to the file
+            current_file.write(str(student_tickets) + " Student tickets\n")
+            current_file.write(str(alumni_tickets) + " Alumni tickets\n")
+            current_file.write(str(general_tickets) + " General tickets\n")
+
+            current_file.close()
+        else:
+            current_file = open(file_name, 'x')
+            #Gets the inputs from the user
+            try:
+                self.student_tickets = int(self.student_ticket_input.get(1.0, 'end-1c'))
+                self.alumni_tickets = int(self.alumni_ticket_input.get(1.0, 'end-1c'))
+                self.general_tickets = int(self.general_ticket_input.get(1.0, 'end-1c'))
+            except ValueError:
+                tkinter.messagebox.showerror("Error", "There must be an input for all ticket fields")
+
+            #writing to the file
+            current_file.write(str(self.student_tickets) + " Student tickets\n")
+            current_file.write(str(self.alumni_tickets) + " Alumni tickets\n")
+            current_file.write(str(self.general_tickets) + " General tickets\n")
+
+            current_file.close()
+
+#takes in a line from the saved data file and gets the number of tickets from it
+    def get_tickets(read_line):
+        #takes the first character of the line
+        current_char = read_line[0]
+
+        #empty list to append to
+        number_list = []
+        counter = 0
+
+        #takes only characters from the line up to the space
+        #as it should only have numbers
+        while(current_char != ' '):
+            number_list.append(current_char)
+            counter += 1
+            current_char = read_line[counter]
+        
+        number = ''
+
+        #convert the numbers into a singular string
+        for i in number_list:
+            number += i
+        
+        #convert the string into a number
+        number = int(number)
+
+        return number
+    
 
 
 #calling gui to be made
